dd835da39fa5f7999789505c0b5e47354f023a42
fghdfgd <115444345+maarifalawi@users.noreply.github.com>
2024-06-15 17:29:01 +0700

    Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,7 +17,8 @@ const Footer = () => {
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          LET'S <span className="text-purple">COLLABORATION!</span>
+          Ready to take <span className="text-purple">your</span> digital
+          presence to the next level?        
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
           Reach out to me today and let&apos;s discuss how I can help you

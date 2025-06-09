5c338762342edd0a24e8ef8d24e7872fd3dff3ca
fghdfgd <115444345+asdfkhjas923485@users.noreply.github.com>
2025-06-09 17:14:27 +0700

    RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -9,8 +9,8 @@ const RecentProjects = () => {
   return (
     <div className="py-20">
       <h1 className="heading">
-        Maarif Alawi's{" "}
-        <span className="text-purple">Certificates</span>
+        2024{" "}
+        <span className="text-purple">Recaps</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map((item) => (
